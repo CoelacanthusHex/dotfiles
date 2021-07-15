978375afa0a9878b3b33d19b18b6c3c4b36a6a82
Coelacanthus <CoelacanthusHex@gmail.com>
2021-07-16 00:15:43 +0800

    refactor(zsh): speed up!
    https://github.com/SukkaW/dotfiles/blob/32284890ea898cb6edc4071d07d1c9f7e799fe3b/_zshrc/macos.zshrc#L622-L637

--- zsh/.zsh.d/plugin.zsh
+++ zsh/.zsh.d/plugin.zsh
@@ -91,6 +91,19 @@ zinit light-mode for \
     atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
 export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
 export ZSH_AUTOSUGGEST_USE_ASYNC=true
+# This speeds up pasting w/ autosuggest
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+# https://github.com/zsh-users/zsh-autosuggestions/issues/351
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste accept-line)
 
 _ZSH_PLUGINS="/usr/share/zsh/plugins"
 _enabled_plugins=(
