f963ad2bf8646d3ab7b42677d359cbc39b2f4d11
Coelacanthus <CoelacanthusHex@gmail.com>
2021-07-30 23:26:14 +0800

    feat(zsh): add function to get commiting time

--- zsh/.zsh.d/function.zsh
+++ zsh/.zsh.d/function.zsh
@@ -237,6 +237,11 @@ get-git-code-line-mail() {
 	fi
 }
 
+# https://maiyang.me/post/2020-08-18-git-commit-history-stat-hour/
+get-someone-commit-time() {
+	git log --author="$1" --date=iso | perl -nalE 'if (/^Date:\s+[\d-]{10}\s(\d{2})/) { say $1+0 }' | sort | uniq -c|perl -MList::Util=max -nalE '$h{$F[1]} = $F[0]; }{ $m = max values %h; foreach (0..23) { $h{$_} = 0 if not exists $h{$_} } foreach (sort {$a <=> $b } keys %h) { say sprintf "%02d - %4d %s", $_, $h{$_}, "*"x ($h{$_} / $m * 50); }'
+}
+
 function foreground-last-job () {
     if (( ${#jobstates} )); then
         zle .push-input
