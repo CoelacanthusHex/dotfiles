434ce3b7dd2c87f7d61b44f792eb3ce171fdd5a4
Coelacanthus <coelacanthus@outlook.com>
2021-11-27 15:17:18 +0800

    zsh: grep add --context=3 --extended-regexp

--- zsh/.config/zsh.d/alias.zsh
+++ zsh/.config/zsh.d/alias.zsh
@@ -139,9 +139,9 @@ export GREP_COLORS="mt=$GREP_COLOR" # GNU.
 alias diff='diff --color=auto'
 alias pactree="pactree -c"
 alias ssh="TERM=xterm-256color ssh"
-alias grep="${aliases[grep]:-grep} --color=auto"
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
+alias grep="${aliases[grep]:-grep} --color=auto --context=3 --extended-regexp"
+alias egrep='egrep --color=auto --context=3'
+alias fgrep='fgrep --color=auto --context=3'
 alias ncdu='ncdu --color dark'
 alias ip="ip -c=auto "
 
