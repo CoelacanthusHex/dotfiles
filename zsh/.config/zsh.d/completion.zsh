0f3acaab04162b81f848f39867ed4e1cb954cf9d
Coelacanthus <coelacanthus@outlook.com>
2021-12-02 09:07:31 +0800

    zsh: add okular completion and mutt address completion

--- zsh/.config/zsh.d/completion.zsh
+++ zsh/.config/zsh.d/completion.zsh
@@ -187,16 +187,18 @@ zstyle ':completion:*:*:x:*' file-patterns \
     '*.(#i)(tar.gz|tgz|tar.bz2|tbz|tbz2|tar.xz|txz|tar.zma|tlz|tar.zst|tzst|tar.lz|tar.lz4|tar.lrz|tar|cbt|gz|bz2|xz|lrz|lz4|lzma|z|Z|zip|war|jar|ear|sublime-package|ipa|ipsw|xpi|apk|aar|whl|cbz|epub|maff|rar|cbr|rpm|7z|chm|cb7|deb|zst|exe|cab|cpio|cba|ace|zpaq|arc)(-.):compressed-files:"compressed files" *(-/):directories:directories'
 
 # remove svg from feh completion list because it need a parameter `--conversion-timeout 1`
-zstyle ':completion:*:*:feh:*' file-patterns '*.(#i)(png|gif|jpg|jpeg)(-.):images:images *(-/):directories:directories'
-zstyle ':completion:*:*:viu:*' file-patterns '*.(#i)(png|gif|jpg|jpeg)(-.):images:images *(-/):directories:directories'
+zstyle ':completion:*:*:feh:*' file-patterns '*.(#i)(png|gif|jpg|jpeg|webp)(-.):images:images *(-/):directories:directories'
+zstyle ':completion:*:*:viu:*' file-patterns '*.(#i)(png|gif|jpg|jpeg|webp)(-.):images:images *(-/):directories:directories'
 # TODO: add *.svg to `display` completion list
 zstyle ':completion:*:*:inkview:*' file-patterns '*.(#i)(svg)(-.):images:"vector images" *(-/):directories:directories'
-_pic_viewer_formats=(png gif jpg jpeg tiff bmp)
+_pic_viewer_formats=(png gif jpg jpeg tiff bmp webp avif)
 zstyle ':completion:*:*:pic-viewer:*' file-patterns "*.(#i)(${(j:|:)_pic_viewer_formats})(-.):images:images *(-/):directories:directories"
 unset _pic_viewer_formats
 zstyle ':completion:*:*:svg-viewer:*' file-patterns '*.(#i)(svg)(-.):images:"vector images" *(-/):directories:directories'
 zstyle ':completion:*:*:mpv:*' file-patterns \
     '*.(#i)(flv|mp4|webm|mkv|wmv|mov|avi|mp3|ogg|wma|flac|wav|aiff|m4a|m4b|m4v|gif|ifo)(-.) *(-/):directories' '*:all-files'
+zstyle ':completion::complete:evince:*' file-patterns '*.{pdf,ps,eps,dvi,djvu,pdf.gz,ps.gz,dvi.gz}:documents:documents *(-/):directories:directories'
+zstyle ':completion::complete:okular:*' file-patterns '*.{pdf,ps,eps,dvi,djvu,pdf.gz,ps.gz,dvi.gz}:documents:documents *(-/):directories:directories'
 
 # ignore for editor
 # https://github.com/MaskRay/Config/blob/master/home/.zshrc#L170
@@ -221,5 +223,7 @@ zstyle ':completion:*:*:(python*|pytest):*:*files' ignored-patterns '*.(pyc)(-.)
 #cmake_langs=('C' 'C' 'CXX' 'C++')
 #zstyle ':completion:*:cmake:*' languages $cmake_langs
 
+# Mutt Address Book
+[[ -r ~/.config/mutt/muttrc.aliases ]] && zstyle ':completion:*:mutt:*' users ${${${(f)"$(<~/.config/mutt/muttrc.aliases)"}#alias[[:space:]]}%%[[:space:]]*}
 
 # vim: ft=zsh sw=4 ts=8 sts=4 et
