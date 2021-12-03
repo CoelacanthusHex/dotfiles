b29e361e367aece9e31434fe58c88037dc144e15
Coelacanthus <coelacanthus@outlook.com>
2021-12-04 01:26:31 +0800

    firefox: always display all parts of URL and some harden like disable non-modern cipher suites

--- firefox/.mozilla/firefox/userprofile/user.js
+++ firefox/.mozilla/firefox/userprofile/user.js
@@ -21,7 +21,8 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("font.name-list.emoji", "emoji");
 /* 显示紧凑模式 */
 user_pref("browser.compactmode.show", "true");
-
+/* 在地址栏显示完整的 URL */
+user_pref("browser.urlbar.trimURLs", "false")
 
 /* 禁止投机性预连接 */
 // user_pref("network.http.speculative-parallel-limit", 0);
@@ -61,3 +62,67 @@ user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffvpx.enabled", false);
 /* 显卡不支持AV1解码 */
 user_pref("media.av1.enabled", false);
+
+/*==== 连接安全 ====*/
+/* https://github.com/arkenfox/user.js/blob/master/user.js#L465 */
+
+/*** [SECTION 1200]: HTTPS (SSL/TLS / OCSP / CERTS / HPKP)
+   Your cipher and other settings can be used in server side fingerprinting
+   [TEST] https://www.ssllabs.com/ssltest/viewMyClient.html
+   [TEST] https://browserleaks.com/ssl
+   [TEST] https://ja3er.com/
+   [1] https://www.securityartwork.es/2017/02/02/tls-client-fingerprinting-with-bro/
+***/
+
+/** SSL (Secure Sockets Layer) / TLS (Transport Layer Security) ***/
+/* 1201: require safe negotiation
+ * Blocks connections (SSL_ERROR_UNSAFE_NEGOTIATION) to servers that don't support RFC 5746 [2]
+ * as they're potentially vulnerable to a MiTM attack [3]. A server without RFC 5746 can be
+ * safe from the attack if it disables renegotiations but the problem is that the browser can't
+ * know that. Setting this pref to true is the only way for the browser to ensure there will be
+ * no unsafe renegotiations on the channel between the browser and the server.
+ * [STATS] SSL Labs (July 2021) reports over 99% of sites have secure renegotiation [4]
+ * [1] https://wiki.mozilla.org/Security:Renegotiation
+ * [2] https://tools.ietf.org/html/rfc5746
+ * [3] https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2009-3555
+ * [4] https://www.ssllabs.com/ssl-pulse/ ***/
+//user_pref("security.ssl.require_safe_negotiation", true);
+/* 1270: display warning on the padlock for "broken security" (if 1201 is false)
+ * Bug: warning padlock not indicated for subresources on a secure page! [2]
+ * [1] https://wiki.mozilla.org/Security:Renegotiation
+ * [2] https://bugzilla.mozilla.org/1353705 ***/
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+/* 1203: reset TLS 1.0 and 1.1 downgrades i.e. session only ***/
+user_pref("security.tls.version.enable-deprecated", false); // [DEFAULT: false]
+
+/** CERTS / HPKP (HTTP Public Key Pinning) ***/
+/* 1220: disable or limit SHA-1 certificates
+ * 0 = allow all
+ * 1 = block all
+ * 3 = only allow locally-added roots (e.g. anti-virus) (default)
+ * 4 = only allow locally-added roots or for certs in 2015 and earlier
+ * [SETUP-CHROME] If you have problems, update your software: SHA-1 is obsolete
+ * [1] https://blog.mozilla.org/security/2016/10/18/phasing-out-sha-1-on-the-public-web/ ***/
+user_pref("security.pki.sha1_enforcement_level", 1);
+
+/* 1272: display advanced information on Insecure Connection warning pages
+ * only works when it's possible to add an exception
+ * i.e. it doesn't work for HSTS discrepancies (https://subdomain.preloaded-hsts.badssl.com/)
+ * [TEST] https://expired.badssl.com/ ***/
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+
+/* 7003: disable non-modern cipher suites [1]
+ * [WHY] Passive fingerprinting. Minimal/non-existent threat of downgrade attacks
+ * [TEST] https://browserleaks.com/ssl ***/
+user_pref("security.ssl3.ecdhe_ecdsa_aes_256_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_aes_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_aes_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_aes_256_sha", false);
+user_pref("security.ssl3.rsa_aes_128_gcm_sha256", false); // no PFS
+user_pref("security.ssl3.rsa_aes_256_gcm_sha384", false); // no PFS
+user_pref("security.ssl3.rsa_aes_128_sha", false); // no PFS
+user_pref("security.ssl3.rsa_aes_256_sha", false); // no PFS
+/* 7004: control TLS versions
+ * [WHY] Passive fingerprinting. Downgrades are still possible: behind user interaction ***/
+   // user_pref("security.tls.version.min", 3); // [DEFAULT: 3] 3 is TLS1.2
+   // user_pref("security.tls.version.max", 4)
