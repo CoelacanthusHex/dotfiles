df89168c174088ba8ad011b4b5e623f07511d3d5
Coelacanthus <coelacanthus@outlook.com>
2021-12-03 23:04:29 +0800

    ssh: add AOSC Buildbot config

--- ssh/POSTINSTALL.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# https://infosec.mozilla.org/guidelines/openssh.html#modern-openssh-67
-# using `awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.tmp && mv /etc/ssh/moduli.tmp /etc/ssh/moduli`
-# https://jbeekman.nl/blog/2015/05/ssh-logjam/
-# using `awk '{ if ($5 <= 2000) printf "#"; print }' /etc/ssh/moduli > /tmp/large_moduli && mv /tmp/large_moduli /etc/ssh/moduli`
-awk '{ if ($5 <= 3071) printf "#"; print }' /etc/ssh/moduli > /etc/ssh/moduli.tmp && mv /etc/ssh/moduli.tmp /etc/ssh/moduli
-echo -e "\n# Restrict key exchange, cipher, and MAC algorithms, as per sshaudit.com\n# hardening guide.\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\nCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\nMACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\nHostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com" >> /etc/ssh/sshd_config
