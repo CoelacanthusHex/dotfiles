3a17d11e80295f8136647298f2b4b54d99b3bea3
Coelacanthus <coelacanthus@outlook.com>
2022-01-21 13:08:22 +0800

    ssh: optimize file structure

--- zsh/.config/zsh.d/completion.zsh
+++ zsh/.config/zsh.d/completion.zsh
@@ -254,9 +254,8 @@ zstyle -e ':completion:*:hosts' hosts 'reply=(
     ${=${=${=${${(f)"$(cat {/etc/ssh/ssh_,~/.ssh/}known_hosts(|2)(N) 2> /dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
     ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2> /dev/null))"}%%(\#${_etc_host_ignores:+|${(j:|:)~_etc_host_ignores}})*}
     ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
-    ${=${${${${(@M)${(f)"$(cat ~/.ssh/config.d/aosc 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
-    ${=${${${${(@M)${(f)"$(cat ~/.ssh/config.d/plct 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
-    ${=${${${${(@M)${(f)"$(cat ~/.ssh/config.d/local 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
+    ${=${${${${(@M)${(f)"$(cat ~/.ssh/config.d/* 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
+    ${=${${${${(@M)${(f)"$(cat ~/.ssh/config.d/**/* 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
 )'
 zstyle ':completion:*:(ssh|scp|rsync|mosh):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
 zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-host hosts-domain hosts-ipaddr
@@ -271,7 +270,7 @@ zstyle ':completion:*:(ssh|scp|rsync|sshfs|mosh):*:hosts-host' ignored-patterns 
 zstyle ':completion:*:(ssh|scp|rsync|sshfs|mosh):*:hosts-domain' ignored-patterns \
     '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*' \
     '*.localdomain' \
-    '*.github*'
+    '*.github*' 'github.com' 'aur.archlinux.org'
 zstyle ':completion:*:(ssh|scp|rsync|sshfs|mosh):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.<->.<->' '255.255.255.255' '::1' 'fe80::*'
 zstyle ':completion:*:(ssh|scp|rsync|sshfs|mosh):*:users' ignored-patterns \
     adm amule amanda apache avahi bin brltty chrony colord courier cups clamav \
