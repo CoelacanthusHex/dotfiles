bb877dd079edb911d6a0f138bd735f8e16a9ffb6
Coelacanthus <coelacanthus@outlook.com>
2022-01-30 16:10:27 +0800

    zsh(plugin/better-man): prefer less with param

--- zsh/.config/zsh.d/plugins/better-man-pages.plugin.zsh
+++ zsh/.config/zsh.d/plugins/better-man-pages.plugin.zsh
@@ -30,29 +30,34 @@ less_termcap[ZW]="${terminfo[rsupm]}"
 typeset __better_man_pages_dir="${0:A:h}"
 
 function __make_better() {
-  local -a environment
+    local -a environment
 
-  # Convert associative array to plain array of NAME=VALUE items.
-  local k v
-  for k v in "${(@kv)less_termcap}"; do
-    environment+=( "LESS_TERMCAP_${k}=${v}" )
-  done
+    # Convert associative array to plain array of NAME=VALUE items.
+    local k v
+    for k v in "${(@kv)less_termcap}"; do
+        environment+=( "LESS_TERMCAP_${k}=${v}" )
+    done
 
-  # Prefer `less` whenever available, since we specifically configured
-  # environment for it.
-  environment+=( PAGER="${commands[less]:-$PAGER}" )
+    # Prefer `less` whenever available, since we specifically configured
+    # environment for it.
+    (( $+commands[less] )) \
+        && environment+=( MANPAGER="less -s -M +Gg" ) \
+        || environment+=( MANPAGER="$PAGER" )
 
-  # See ./nroff script.
-  if [[ "$OSTYPE" = solaris* ]]; then
-    environment+=( PATH="${__colored_man_pages_dir}:$PATH" )
-  fi
+    # See ./nroff script.
+    if [[ "$OSTYPE" = solaris* ]]; then
+        environment+=( PATH="${__colored_man_pages_dir}:$PATH" )
+    fi
 
-  command env $environment "$@"
+    command env $environment "$@"
 }
 
 # Colorize man and dman/debman (from debian-goodies)
 function man \
-  dman \
-  debman {
-  __make_better $0 "$@"
+    dman \
+    debman {
+    __make_better $0 "$@"
 }
+
+# vim: ft=zsh sw=4 ts=8 sts=4 et:
+# kate: space-indent on; indent-width 4;
