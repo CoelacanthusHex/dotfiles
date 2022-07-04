66d2f5a626023727caea4431e7a41528c2b02cf6
Coelacanthus <coelacanthus@outlook.com>
2022-07-04 15:45:50 +0800

    zsh(completion): add more command to editor ignore pattern

--- zsh/.config/zsh.d/completion.d/editor.zsh
+++ zsh/.config/zsh.d/completion.d/editor.zsh
@@ -1,12 +1,16 @@
 # ignore for editor
 # https://github.com/MaskRay/Config/blob/master/home/.zshrc#L170
 # ignore video files, audio files and compiled files
+# https://github.com/romkatv/zsh4humans/blob/fd101ce997d8434668af517cc159e3ab42c0ff30/fn/-z4h-compinit#L13
+local -aU editors=(
+  vi vim nvim emacs nano gedit code kak kate mcedit joe $EDITOR $VISUAL
+  bat cat less more $PAGER)
 _ignore_video_files=(avi mkv rmvb wmv mp4 flv webm mov)
 _ignore_audio_files=(mp3 flac ogg wav)
 _ignore_compiled_files=(a dylib so rlib lib dll o pyc zwc zwc.old)
 _ignore_files=()
 _ignore_files+=($_ignore_video_files) && _ignore_files+=($_ignore_audio_files) && _ignore_files+=($_ignore_compiled_files) && \
-zstyle ':completion:*:*:(vi|vim|nvim|emacs|nano):*:*files' ignored-patterns \
+zstyle ':completion:*:*:('${(j:|:)editors}'):*:*' ignored-patterns \
     "*.(#i)(${(j:|:)_ignore_files})"
 
 # vim: ft=zsh sw=4 ts=8 sts=4 et:
