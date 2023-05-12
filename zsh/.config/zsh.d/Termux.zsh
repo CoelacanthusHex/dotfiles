5cc8f612671a190156ed883193131a7eb597dd20
Coelacanthus <CoelacanthusHex@gmail.com>
2023-05-13 00:48:34 +0800

    zsh(env/termux): add okc-ssh-agent init
    Signed-off-by: Coelacanthus <CoelacanthusHex@gmail.com>

--- zsh/.config/zsh.d/Termux.zsh
+++ zsh/.config/zsh.d/Termux.zsh
@@ -2,5 +2,7 @@ alias xdg-open='termux-open'
 alias pbcopy='termux-clipboard-set'
 alias pbpaste='termux-clipboard-get'
 
+(( $+commands[okc-ssh-agent] )) && eval $(okc-ssh-agent) &> /dev/null
+
 # vim: ft=zsh sw=4 ts=8 sts=4 et:
 # kate: space-indent on; indent-width 4; syntax zsh;
