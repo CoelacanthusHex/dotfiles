31f4d7006ac97ed7145116fb0482f9d8cf0ce2aa
Coelacanthus <uwu@coelacanthus.name>
2025-09-24 18:47:33 +0800

    gdb: update firefox symbol script

--- gdb/.config/gdb/firefox-symbols.py
+++ gdb/.config/gdb/firefox-symbols.py
@@ -6,29 +6,47 @@
 # To use, run `source /path/to/symbols.py` in GDB 7.9 or newer, or
 # put that in your ~/.gdbinit.
 
-from __future__ import print_function
 
+import gzip
+import io
 import itertools
 import os
-import subprocess
-from urllib.parse import urljoin, quote
+import shutil
 
-SYMBOL_SERVER_URL = 'https://s3-us-west-2.amazonaws.com/org.mozilla.crash-stats.symbols-public/v1/'
+try:
+    from urllib.error import HTTPError, URLError
+    from urllib.parse import quote, urljoin
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import quote
 
-debug_dir = os.path.join(os.environ['HOME'], '.cache', 'gdb')
-cache_dir = os.path.join(debug_dir, '.build-id')
+    from urllib2 import urlopen
+    from urlparse import urljoin
+
+SYMBOL_SERVER_URL = "https://symbols.mozilla.org/"
+
+debug_dir = os.path.join(os.environ["HOME"], ".cache", "gdb")
+cache_dir = os.path.join(debug_dir, ".build-id")
+
 
 def munge_build_id(build_id):
-    '''
+    """
     Breakpad stuffs the build id into a GUID struct so the bytes are
     flipped from the standard presentation.
-    '''
-    b = list(map(''.join, list(zip(*[iter(build_id.upper())]*2))))
-    return ''.join(itertools.chain(reversed(b[:4]), reversed(b[4:6]),
-                                   reversed(b[6:8]), b[8:16])) + '0'
+    """
+    b = list(map("".join, list(zip(*[iter(build_id.upper())] * 2))))
+    return (
+        "".join(
+            itertools.chain(
+                reversed(b[:4]), reversed(b[4:6]), reversed(b[6:8]), b[8:16]
+            )
+        )
+        + "0"
+    )
+
 
 def try_fetch_symbols(filename, build_id, destination):
-    debug_file = os.path.join(destination, build_id[:2], build_id[2:] + '.debug')
+    debug_file = os.path.join(destination, build_id[:2], build_id[2:] + ".debug")
     if os.path.exists(debug_file):
         return debug_file
     try:
@@ -37,41 +55,50 @@ def try_fetch_symbols(filename, build_id, destination):
             os.makedirs(d)
     except OSError:
         pass
-    path = os.path.join(filename, munge_build_id(build_id), filename + '.dbg.gz')
+    path = os.path.join(filename, munge_build_id(build_id), filename + ".dbg.gz")
     url = urljoin(SYMBOL_SERVER_URL, quote(path))
     try:
-        print('Fetching symbols from {0}'.format(url))
-        cmd = f'wget --retry-connrefused --retry-on-host-error -T5 -t100 -c --no-use-server-timestamps "{url}" -O- | gunzip > "{debug_file}"'
-        subprocess.check_call(cmd, shell=True)
-        return debug_file
-    except:
-        return None
+        u = urlopen(url)
+        if u.getcode() != 200:
+            return None
+        print(f"Fetching symbols from {url}")
+        with open(debug_file, "wb") as f, gzip.GzipFile(
+            fileobj=io.BytesIO(u.read()), mode="r"
+        ) as z:
+            shutil.copyfileobj(z, f)
+            return debug_file
+    except (URLError, HTTPError):
+        None
 
 
 def is_moz_binary(filename):
-    '''
+    """
     Try to determine if a file lives in a Firefox install dir, to save
     HTTP requests for things that aren't going to work.
-    '''
+    """
     # The linux-gate VDSO doesn't have a real filename.
     if not os.path.isfile(filename):
         return False
     while True:
         filename = os.path.dirname(filename)
-        if filename == '/':
+        if filename == "/" or not filename:
             return False
-        if os.path.isfile(os.path.join(filename, 'application.ini')):
+        if os.path.isfile(os.path.join(filename, "application.ini")):
             return True
 
 
 def fetch_symbols_for(objfile):
-    build_id = objfile.build_id if hasattr(objfile, 'build_id') else None
-    if getattr(objfile, 'owner', None) is not None or any(o.owner == objfile for o in gdb.objfiles()):
+    build_id = objfile.build_id if hasattr(objfile, "build_id") else None
+    if getattr(objfile, "owner", None) is not None or any(
+        o.owner == objfile for o in gdb.objfiles()
+    ):
         # This is either a separate debug file or this file already
         # has symbols in a separate debug file.
         return
     if build_id and is_moz_binary(objfile.filename):
-        debug_file = try_fetch_symbols(os.path.basename(objfile.filename), build_id, cache_dir)
+        debug_file = try_fetch_symbols(
+            os.path.basename(objfile.filename), build_id, cache_dir
+        )
         if debug_file:
             objfile.add_separate_debug_file(debug_file)
 
@@ -81,11 +108,12 @@ def new_objfile(event):
 
 
 def fetch_symbols():
-    '''
+    """
     Try to fetch symbols for all loaded modules.
-    '''
+    """
     for objfile in gdb.objfiles():
         fetch_symbols_for(objfile)
+
 
 # Create our debug cache dir.
 try:
@@ -96,11 +124,12 @@ except OSError:
 
 # Set it as a debug-file-directory.
 try:
-    dirs = gdb.parameter('debug-file-directory').split(':')
+    dirs = gdb.parameter("debug-file-directory").split(":")
 except gdb.error:
     dirs = []
 if debug_dir not in dirs:
     dirs.append(debug_dir)
-    gdb.execute('set debug-file-directory %s' % ':'.join(dirs))
+    gdb.execute("set debug-file-directory {}".format(":".join(dirs)))
 
 gdb.events.new_objfile.connect(new_objfile)
+
